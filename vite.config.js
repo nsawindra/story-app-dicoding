e37e00b737b587248b42e5cb9bc0da478ce00b42
Nabilah Putri Sawindra <nsawindra@gmail.com>
2025-07-06 17:19:29 +0700

    fix: tambahkan icon 144px untuk installability

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,11 @@ export default defineConfig({
         theme_color: '#34495e',
         icons: [
           {
+            src: 'icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
             src: 'icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
@@ -25,7 +30,8 @@ export default defineConfig({
           {
             src: 'icons/icon-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
